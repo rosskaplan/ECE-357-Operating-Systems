0092e562629b0ee239bfd62c589bb413fb5fa411
rosskaplan <ross.kaplan96@gmail.com>
2016-09-21 17:45:47 -0400

    Adding in error checking for partial writes and invalid buffer sizes

--- copycat.c
+++ copycat.c
@@ -4,13 +4,17 @@
 #include <fcntl.h> //Defines O_APPEND, O_CREAT, etc. as constants
 #include <errno.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <sys/types.h> //Necessary on some operating systems
+#include <sys/stat.h> //Necessary on some operating systems
+#include <time.h> //Used for timing the program
 
 void processInput(int, char **, int, int, char*);
 void errorOutput(char*, char*, char*);
 
 int main(int argc, char **argv) {
+
+    clock_t t;
+    t = clock();
 
     if (argc == 1) {
         printf("You didn't provide any input or output files!\n");
@@ -21,6 +25,8 @@ int main(int argc, char **argv) {
     int start = 1;
     if (strcmp(argv[1], "-b") == 0) {
         bytes = strtol(argv[2], NULL, 10);
+        if (bytes < 2)
+            errorOutput("Buffer size inputted is not valid","", "Please enter something larger");
         start = 3; 
         if ((argc > 5) && strcmp(argv[3], "-o") == 0) {
             outputfile = argv[4];
@@ -32,18 +38,22 @@ int main(int argc, char **argv) {
         start = 3;
     }
     processInput(argc, argv, start, bytes, outputfile);
-    
+   
+    t = clock() - t;
+    printf ("%f \n",((float)t)/CLOCKS_PER_SEC);
+
 return 0;
 }
 
 void processInput(int argc, char** argv, int start, int bytes, char* outputfile) {
     bytes = (bytes == -1)?1024:bytes;
     char dataarr[bytes+1];
+    dataarr[bytes] = '\0';
     int fdw, byteswritten;
     fdw = open(outputfile, O_CREAT | O_WRONLY | O_TRUNC, 0777);
     if (!outputfile) fdw = STDOUT_FILENO;
     if (fdw < 0 && !(!outputfile)) errorOutput("Function call open failed to open ", (!outputfile) ? "stdout" : outputfile, strerror(errno));
-    int fdin, rbytes;
+    int fdin, rbytes, attemptwrite;
     for (int i = start; i < argc; i++) {
         if (strcmp(argv[i],"-") == 0) {
             fdin = STDIN_FILENO; 
@@ -55,8 +65,14 @@ void processInput(int argc, char** argv, int start, int bytes, char* outputfile)
             byteswritten =  write(fdw, dataarr, rbytes);
             if (byteswritten < 0)
                 errorOutput("Function call write failed to ", (!outputfile) ? "stdout" : outputfile, strerror(errno));
-            else if(byteswritten != rbytes)
-                errorOutput("Function call write was only partially written to ", (!outputfile) ? "stdout" : outputfile, strerror(errno));
+            else if(byteswritten != rbytes) {
+                attemptwrite = write(fdw, &dataarr[byteswritten], bytes-byteswritten);
+                if ((attemptwrite + byteswritten) == rbytes) {
+                    fprintf(stderr, "Warning: Partial write occurred, but it was able to write remainder\n");
+                } else {
+                    errorOutput("Function call write was only partially written to ", (!outputfile) ? "stdout" : outputfile, strerror(errno));
+                }
+            }
         }
         if (rbytes < 0) {
             errorOutput("Function call read failed from ", argv[i], strerror(errno));
