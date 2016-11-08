750d38175ca47b392b40f9b452e7a6181f90bf03
rosskaplan <ross.kaplan96@gmail.com>
2016-11-08 00:14:45 -0500

    Beginning of stat and mmap commands

--- project5/bgrep.c
+++ project5/bgrep.c
@@ -3,17 +3,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
 
 #define PATTERN_MAX 2048
 
+int findPattern(int, char*, char**, int);
+
 int main(int argc, char **argv) {
-    int context;
+    int context = 0;
     char *pattern = malloc(sizeof(char)*(PATTERN_MAX+1));
+    pattern = "\0";
     char ch;
     while ((ch = getopt(argc, argv, "c:p:")) != -1) {
         switch(ch) {
             case 'c':
-                printf("here");
                 if (sscanf(optarg, "%d", &context) != 1) { //If it gets a string
                     fprintf(stderr, "Invalid argument: %s. Usage: -c should precede an integer.\n", optarg);
                     return -1;
@@ -32,12 +36,27 @@ int main(int argc, char **argv) {
                 break;
         }
     }
-    printf("optind: %d", optind);
-    char** files;
-    for (int i = 0; i < argc-optind; i++) {
-        files[i] = argv[optind+i-1];
+    char** files = malloc(sizeof(char)*(argc-optind)*(PATTERN_MAX+1));
+    int i = 0;
+    for (; i < argc-optind; i++) {
+        files[i] = argv[optind+i];
     }
-    printf("%d %s %s %s", context, pattern, files[0], files[1]);
+
+if (findPattern(context, pattern, files, i) == -1)
+    return -1;
 
 return 0;
+}
+
+int findPattern(int context, char* pattern, char** files, int numfiles) {
+
+    struct stat stat_struct;
+    //Use the length of the file to get the block of memory
+    for (int i = 0; i < numfiles; i++) {
+        if (lstat(files[i], &stat_struct) != 0) {
+            fprintf(stderr, "Failed to get stat struct on %s: %s\n", files[i], strerror(errno));
+            return -1;
+        }
+    int p = mmap(NULL, stat_struct.st_size, 
+
 }
