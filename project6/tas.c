a31eae996772946aeb8f4119e2edc45f81e70ba4
Ross Kaplan <ross.kaplan96@gmail.com>
2016-11-30 00:13:33 -0500

    made a makefile, updated small change in tas.c

--- project6/tas.c
+++ project6/tas.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <errno.h>
-#include <fcntl.h>
 #include <signal.h>
 #include <sys/mman.h>
 #include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main(int argc, char **argv) {
 
     pid_t pid; 
-    if ((long * test = mmap(NULL, 4096, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANON, 0, 0)) != 0) {
+    long* test;
+    if ((test = mmap(NULL, 4096, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANON, 0, 0)) != 0) {
         fprintf(stderr, "Error mapping.  Error code: %s\n", strerror(errno));
         exit(-1);
     }
