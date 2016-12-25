eb4c12cec176c5f48eb16c52528dc56a2d8080c6
rosskaplan <ross.kaplan96@gmail.com>
2016-12-25 11:26:22 -0500

    finished assuming we should be working in ICANON. oherwise, will be changed

--- project9/drivers.c
+++ project9/drivers.c
@@ -7,6 +7,8 @@
 
 #define MAX_STRING_LENGTH 2049 //Includes null terminate
 
+int my_lineread(void);
+
 int main(int argc, char** argv, char** envp) {
 
     struct termios term;
@@ -14,7 +16,7 @@ int main(int argc, char** argv, char** envp) {
         fprintf(stderr, "Error: tcgetattr failed. Error code: %s.\n", strerror(errno));
         exit(-1);
     }
-    term.c_lflag &= ~(ICANON | ECHO);
+    term.c_lflag &= (ICANON | ECHO);
     term.c_cc[VMIN] = 1;
     term.c_cc[VTIME] = 0;
 
@@ -25,51 +27,44 @@ int main(int argc, char** argv, char** envp) {
 
     while (my_lineread()) {
         printf("Read a newline, it was echoed back and we're onto the next one!\n");
-        return 0;
     }
     printf("Read an end of file. We're done!\n");
     return 0;
 
 }
 
-int my_lineread(int eof_check, string inputline) {
+int my_lineread(void) {
 
     char c;
-    int byte;
     char buf[MAX_STRING_LENGTH];
     int length = 0;
-    while((byte = read(0, &c, 1)) > 0) {
-        if (strcmp(c, termios.c_cc[VERASE]) == 0) { //Strings match
+    while(read(0, &c, 1) > 0) {
+        if (c == VERASE) {
             if (length == 0)
                 continue;
             length--;
-            //abcd, length = 4
-            //length = 3, abc --> remove char at pos 3
             buf[length] = '\0';
-        } else if (strcmp(c, termios.c_cc[VWERASE]) == 0) {
+        } else if (c == VWERASE) {
             while(length != 0 && buf[length] != ' ' && buf[length] != '\t') {
                 buf[length] = '\0';
                 length--;
-                //abcd abcd, length = 8
-                //then abcd abc\0, length = 7;
-                //then abcd ab\0
-                //abcd a\0
-                //abcd a\0
             }
             buf[length] = '\0';
-            length--;
-        } else if (strcmp(c, termios.c_cc[VKILL]) == 0) {
+        } else if (c == VKILL) {
             while(length != 0) {
                 buf[length] = '\0';
                 length--;
             }
             buf[length] = '\0';
-        } else if (strcmp(c, termios.c_cc[VEOF]) == 0) {
-            buf[length] = '\0';
-            return 0;
+        } else if (c == '\n') {
+            buf[length++] = '\n';
+            buf[length++] = '\0';
+            write(1, buf, length);
+            return 1;
         } else {
             buf[length++] = c;
         }
     }
-return 1;
+    //Hit end of file, exit
+    return 0;
 }
